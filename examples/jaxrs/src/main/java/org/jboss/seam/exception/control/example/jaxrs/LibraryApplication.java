f64c4d9a4724ebd53aa5ad431f2ae1f40b674cb8
LightGuard <lightguard.jp@gmail.com>
2010-11-29 16:21:29 -0700

    Updating licence
    Also some minor formatting is being updated

--- examples/jaxrs/src/main/java/org/jboss/seam/exception/control/example/jaxrs/LibraryApplication.java
+++ examples/jaxrs/src/main/java/org/jboss/seam/exception/control/example/jaxrs/LibraryApplication.java
@@ -1,36 +1,32 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright 2010, Red Hat, Inc., and individual contributors
+ * Copyright [2010], Red Hat, Inc., and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.jboss.seam.exception.control.example.jaxrs;
 
-import org.jboss.seam.exception.control.example.jaxrs.handler.CatchBridge;
-import org.jboss.seam.exception.control.example.jaxrs.resource.AuthorResource;
-import org.jboss.seam.exception.control.example.jaxrs.resource.BookResource;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import org.jboss.seam.exception.control.example.jaxrs.handler.CatchBridge;
+import org.jboss.seam.exception.control.example.jaxrs.resource.AuthorResource;
+import org.jboss.seam.exception.control.example.jaxrs.resource.BookResource;
 
 @ApplicationPath("/api")
 public class LibraryApplication extends Application
--- impl/src/main/java/org/jboss/seam/exception/control/ExceptionHandlerDispatch.java
+++ impl/src/main/java/org/jboss/seam/exception/control/ExceptionHandlerDispatch.java
@@ -1,37 +1,33 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright 2010, Red Hat, Inc., and individual contributors
+ * Copyright [2010], Red Hat, Inc., and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.jboss.seam.exception.control;
 
-import org.jboss.seam.exception.control.extension.CatchExtension;
-
-import javax.enterprise.context.spi.CreationalContext;
-import javax.enterprise.event.Observes;
-import javax.enterprise.inject.spi.BeanManager;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.Stack;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.BeanManager;
+
+import org.jboss.seam.exception.control.extension.CatchExtension;
 
 /**
  * Observer of {@link org.jboss.seam.exception.control.ExceptionToCatchEvent} events and handler dispatcher.
