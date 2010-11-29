9a22d880d56d577e58bb3facbb14d5e5d58f0767
Dan Allen <dan.j.allen@gmail.com>
2010-11-28 22:42:46 -0500

    switch to Instance just to be safe

--- examples/jaxrs/src/main/java/org/jboss/seam/exception/control/example/jaxrs/handler/declarative/ExceptionResponseServiceHandler.java
+++ examples/jaxrs/src/main/java/org/jboss/seam/exception/control/example/jaxrs/handler/declarative/ExceptionResponseServiceHandler.java
@@ -2,6 +2,7 @@ package org.jboss.seam.exception.control.example.jaxrs.handler.declarative;
 
 import java.lang.reflect.Method;
 
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
@@ -13,8 +14,9 @@ import org.jboss.seam.rest.exceptions.ErrorMessageWrapper;
 
 public class ExceptionResponseServiceHandler
 {
+   // does this have to be Instance?
    @Inject @CatchResource
-   private ResponseBuilder builder;
+   private Instance<ResponseBuilder> builderProvider;
    
    @AroundInvoke
    public Object processException(InvocationContext ctx)
@@ -22,6 +24,7 @@ public class ExceptionResponseServiceHandler
       Method m = ctx.getMethod();
       if (ctx.getParameters().length > 0 && ctx.getParameters()[0] instanceof CaughtException)
       {
+         ResponseBuilder builder = builderProvider.get();
          CaughtException<?> c = (CaughtException<?>) ctx.getParameters()[0];
          if (m.isAnnotationPresent(SendHttpResponse.class))
          {
