700fcdd5f8b3075f0183b25d65620065c201ffb3
LightGuard <lightguard.jp@gmail.com>
2011-08-09 02:05:50 -0600

    Fixing up tests to not use maven in shrinkwrap

--- testsuite/common/src/main/java/org/jboss/seam/exception/control/test/BaseWebArchive.java
+++ testsuite/common/src/main/java/org/jboss/seam/exception/control/test/BaseWebArchive.java
@@ -16,8 +16,13 @@
  */
 package org.jboss.seam.exception.control.test;
 
-import java.util.Collection;
-import java.util.HashSet;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.zip.ZipFile;
 
 import javax.enterprise.inject.spi.Extension;
 
@@ -25,30 +30,68 @@ import org.jboss.seam.exception.control.CaughtException;
 import org.jboss.seam.exception.control.extension.CatchExtension;
 import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.Asset;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.UrlAsset;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.importer.ZipImporter;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
-import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
 
 /**
  * @author <a href="http://community.jboss.org/people/LightGuard">Jason Porter</a>
  */
 public final class BaseWebArchive {
-    private static Collection<JavaArchive> libraries = new HashSet<JavaArchive>(5);
+    private static volatile JavaArchive solderJar = null;
+    private static final String SOLDER_VERSION = "3.0.0.Final";
+    private static final String SOLDER_NAME = "seam-solder-" + SOLDER_VERSION + ".jar";
+    private static final String SOLDER_PATH = "target" + File.separator;
+    private static final String JBOSS_REPO = "http://repository.jboss.org/nexus/content/groups/public/org/jboss/seam/solder/seam-solder/";
+    private static final String SOLDER_URL_STRING = JBOSS_REPO + SOLDER_VERSION + "/" + SOLDER_NAME;
 
-    public static WebArchive createBase(final String name) {
-        if (libraries.isEmpty()) {
-            // The JBoss Repository must be defined in the user's settings.xml for this to work.
-            libraries.addAll(DependencyResolvers.use(MavenDependencyResolver.class)
-                    .artifacts("org.jboss.seam.solder:seam-solder:3.0.0.Final").resolveAs(JavaArchive.class));
+    public synchronized static WebArchive createBase(final String name) {
+        // Look to see if we have solder saved already
+        if (solderJar == null) {
+            if (new File(SOLDER_PATH + SOLDER_NAME).exists()) {
+                ZipFile solder_jar_zip = null;
+                try {
+                    solder_jar_zip = new ZipFile(SOLDER_PATH + SOLDER_NAME);
+                    solderJar = ShrinkWrap.create(ZipImporter.class, SOLDER_NAME).importFrom(solder_jar_zip).as(JavaArchive.class);
+                } catch (IOException ioe) {
+                    // Not there or couldn't open it, go fetch solder
+                    // Not sure how we'd ever get here, honestly
+                } finally {
+                    try {
+                        if (solder_jar_zip != null) {
+                            solder_jar_zip.close();
+                        }
+                    } catch (IOException e) {
+                        // Swallow
+                    }
+                }
+            } else {
+                try {
+                    final URL solderUrl = new URL(SOLDER_URL_STRING);
+                    final Asset solderAsset = new UrlAsset(solderUrl);
+
+                    // Save it off for future fast opening done above
+                    solderJar = ShrinkWrap.create(ZipImporter.class).importFrom(solderAsset.openStream()).as(JavaArchive.class);
+
+                    final FileOutputStream solderOutputStream = new FileOutputStream(new File(SOLDER_PATH, SOLDER_NAME));
+                    solderJar.as(ZipExporter.class).exportTo(solderOutputStream);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException("Unable to retrieve solder", e);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException("Unable to export solder jar", e);
+                }
+            }
         }
 
         return ShrinkWrap.create(WebArchive.class, name)
                 .addPackage(CaughtException.class.getPackage())
                 .addClass(CatchExtension.class)
                 .addAsServiceProvider(Extension.class, CatchExtension.class)
-                .addAsLibraries(libraries)
+                .addAsLibraries(solderJar)
                 .addAsWebInfResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"));
     }
 }
